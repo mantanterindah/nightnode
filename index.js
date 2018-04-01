05748ac0ed382ab3bbf5c2e6ba982ff93e2ab51d
mantanterindah <tenuz99@gmail.com>
2018-04-01 09:26:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const Discord = require('discord.js');
 const Nightmare = require('nightmare');
 const electron = require('electron');
 const client = new Discord.Client();
-
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 
 var nightmare = new Nightmare({show:false});
 
